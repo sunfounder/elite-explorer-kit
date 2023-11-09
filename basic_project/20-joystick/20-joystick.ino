cb3d9e268d89f84bad32c7b05efea9af156940fc
wulu <wulu@wulu.zone>
2023-11-09 17:28:39 +0800

    Update joystick example

--- basic_project/20-joystick/20-joystick.ino
+++ basic_project/20-joystick/20-joystick.ino
@@ -10,8 +10,10 @@
 
 const int xPin = A0;  //the VRX attach to
 const int yPin = A1;  //the VRY attach to
+const int swPin = 8;  //the SW attach to
 
 void setup() {
+  pinMode(swPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
@@ -19,5 +21,8 @@ void loop() {
   Serial.print("X: ");
   Serial.print(analogRead(xPin), DEC);  // print the value of VRX in DEC
   Serial.print("|Y: ");
-  Serial.println(analogRead(yPin), DEC);  // print the value of VRX in DEC
+  Serial.print(analogRead(yPin), DEC);  // print the value of VRX in DEC
+  Serial.print("|Z: ");
+  Serial.println(digitalRead(swPin));  // print the value of SW
+  delay(50);
 }
